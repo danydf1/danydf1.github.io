20ee40ef158c2702e7702d7bbe0cf54514caed18
DanielFernandez19 <reitmatch@hotmail.com>
2025-03-25 20:13:13 -0300

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://danydf1.github.io/',
-  base: '/danydf1.github.io',
+  base: 'danydf1.github.io',
   vite: {
     plugins: [tailwindcss()]
   }
